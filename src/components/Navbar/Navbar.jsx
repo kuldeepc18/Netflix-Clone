572b398a75aebf68d6ef5d05b91d14de716b000f
Kuldeep Choudhary <choudharykuldeep1803@gmail.com>
2025-07-14 18:59:35 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
             </div>
             <div className="navbar-right">
                 <img src={search_icon} alt="" className='icons'/>
-                <p>Children</p>
+                <p>Search</p>
                 <img src={bell_icon} alt="" className='icons'/>
                 <div className="navbar-profile">
                     <img src={profile_img} alt="" className='profile'/>
